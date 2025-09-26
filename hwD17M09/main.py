263f5bcc2c2daff4f078c24475b54e7e927e9730
Setsumi <butkovvlad2001@gmail.com>
2025-09-26 15:31:23 +0300

    Домашняя работа. Тема: Корректировка кода

--- hwD17M09/main.py
+++ hwD17M09/main.py
@@ -12,20 +12,18 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 template = Jinja2Templates(directory="template")
 
-pwd = ""
-
 @app.get("/", response_class=HTMLResponse)
 async def show_main_page(request: Request):
     return template.TemplateResponse(
         "./index.html",
         {"request": request}
     )
-@app.post("/")
-async def get_pass(request: Request):
+@app.post("/api")
+async def get_pass():
     char = string.ascii_letters + string.digits + string.punctuation
     password = "".join(secrets.choice(char) for _ in range(10))
     return {"pass": password}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8888,
+    uvicorn.run("main:app", host="127.0.0.1", port=8000,
                 reload=True, reload_dirs=["./static", "./template"])
--- hwD17M09/static/script.js
+++ hwD17M09/static/script.js
@@ -2,23 +2,9 @@ const button = document.querySelector("#button_gen");
 const passwordInp = document.querySelector("#password");
 
 button.addEventListener("click", () => {
-    fetch('/', {method: "POST", header: {"content-applications": "application/json"}})
+    fetch('/api', {method: "POST", header: {"content-type": "application/json"}})
     .then((response)=> response.json())
     .then((data) => {
         passwordInp.value = data.pass
     })
 });
-
-
-//document.addEventListener("DOMContentLoaded", function() {
-//    const button = document.querySelector("#button_gen");
-//    const password = document.querySelector("#password");
-//
-//
-//
-//    button.addEventListener("click", (e) => {
-//        let newPassword = generate()
-//        console.log(`Пароль сгенерирован: ${newPassword}`);
-//        password.value = `${newPassword}`;
-//    });
-//});
